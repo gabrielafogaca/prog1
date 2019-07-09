0f92f82348fdc7b22276c4a1de98d3dd6d80a2f8
gabrielafogaca <48135811+gabrielafogaca@users.noreply.github.com>
2019-07-09 10:56:07 -0300

    Add files via upload

---
+++ strings.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+		<meta charset="utf-8">
+<title>Calculo</title>
+</head>
+
+<body>
+
+
+
+	<?php
+
+	$fcn = $_POST['fcn'];
+    $texto = $_POST['texto'];
+
+
+    print ($fcn($texto));
+
+?>
+
+</body>
+</html>
